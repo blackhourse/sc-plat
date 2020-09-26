aed1f32da81ecfd7ef6c8b0ede9885d67f1bb0e6
mait <285182060@qq.com>
2020-09-26 22:42:54 +0800

    Initial commit

---
+++ uaa/src/main/java/com/central/CentralPlatApplication.java
@@ -0,0 +1,13 @@
+package com.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralPlatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralPlatApplication.class, args);
+    }
+
+}
---
+++ uaa/src/test/java/com/central/CentralPlatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.central;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralPlatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
